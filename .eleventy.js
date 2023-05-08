65789d4af3ba56354f804e6b37efed0a3243badf
iamlegh <hello@iamlegh.com>
2023-05-08 11:17:48 +0200

    correction typo

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+const { DateTime } = require ("luxon");
+
+module.exports = function(eleventyConfig) {
+    
+        // définir les chemin des assets
+        eleventyConfig.addPassthroughCopy('./src/style.css');
+        eleventyConfig.addPassthroughCopy('./src/assets');
+
+        eleventyConfig.addFilter("postDate", (dateObj) => {
+            return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+        })
+        //eleventyConfig.addFilter("year", () => `${new Date().getFullYear()}`); // {% year %}
+
+        //définition des répertoires de travail :
+        return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+        
+    };
+}
